64c9ecee16c8dc53a3845fedd06fc8aa742d307f
ZindeIvan <zinde.ivan@yandex.ru>
2021-04-06 18:25:54 +0600

    init GBMaps

---
+++ GBMaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBMaps
+//
+//  Created by Зинде Иван on 4/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
